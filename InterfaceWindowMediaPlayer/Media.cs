1cee56c591bf4f16a499439dc7b850b696d08c51
BloubTek <morgane.ferrandis@epitech.eu>
2015-01-10 20:13:31 +0100

    ajout de la taglib + recuperation des tags, affichage a faire

--- InterfaceWindowMediaPlayer/Media.cs
+++ InterfaceWindowMediaPlayer/Media.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Windows.Forms;
 
 namespace InterfaceWindowMediaPlayer
 {
@@ -15,20 +16,22 @@ namespace InterfaceWindowMediaPlayer
         public string path { get; set; }
         public string name { get; set; }
         public MediaType mediaType { get; set; }
+        public uint trackNb { get; set; }
+        public string[] artists { get; set; }
+        public string album { get; set; }
 
         public Media()
         { 
         
         }
 
-        public Media(string path)
+        public Media(string mypath)
         {
             string tmpImage = ".jpg;.png;.bmp;.jpeg;.JPG";
             string tmpVideo = ".wmv;.mp4;.avi;";
             string tmpMusic = ".mp3;.aac;.ogg;.wav;";
-
-            this.path = path;
-            this.name = Path.GetFileNameWithoutExtension(path);
+            
+            this.path = mypath;
             if (tmpImage.IndexOf(Path.GetExtension(path)) >= 0)
                 this.mediaType = Media.MediaType.IMAGE;
             if (tmpVideo.IndexOf(Path.GetExtension(path)) >= 0)
@@ -36,9 +39,21 @@ namespace InterfaceWindowMediaPlayer
             if (tmpMusic.IndexOf(Path.GetExtension(path)) >= 0)
                 this.mediaType = Media.MediaType.MUSIC;
 
-           
-
-        }
+            try
+            {
+                TagLib.File file = TagLib.File.Create(mypath);
+                if ((name = file.Tag.Title) == null)
+                    name = Path.GetFileNameWithoutExtension(path);
+                trackNb = file.Tag.Track;
+                album = file.Tag.Album;
+                artists = file.Tag.Performers;
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("The specified file does not exist");
+                throw;
+            }
+       }
     }
 
 }
